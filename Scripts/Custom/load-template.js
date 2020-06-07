cb21ba9176de94c988d5bfffc857a3885e9f19ca
Akash-Varude <akashvarude305@gmail.com>
2020-06-07 13:58:53 +0530

    Updated template file created this as a single page web applicaton

---
+++ Scripts/Custom/load-template.js
@@ -0,0 +1,9 @@
+﻿
+
+function load_blogs() {
+    $("#pages-placeholder").load("../../Pages/Site/blogs.html");
+}
+function load_home() {
+    $("#pages-placeholder").load("../../Pages/Site/home.html");
+}
+
