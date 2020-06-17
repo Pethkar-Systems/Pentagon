728651c65c6a5529ab44fd5a997dd0de5aec5549
AvantiVarude <avantivarude2000@gmail.com>
2020-06-17 14:21:32 +0530

    projects page

--- Scripts/Custom/load-template.js
+++ Scripts/Custom/load-template.js
@@ -1,9 +1,13 @@
 ﻿
 
-function load_blogs() {
-    $("#pages-placeholder").load("../../Pages/Site/blogs.html");
-}
 function load_home() {
     $("#pages-placeholder").load("../../Pages/Site/home.html");
+}
+function load_projects() {
+    $("#pages-placeholder").load("../../Pages/Site/projects.html");
+}
+
+function load_blogs() {
+    $("#pages-placeholder").load("../../Pages/Site/blogs.html");
 }
 
