fe1100a34184c726daad4e7d5030cd8c9a40f714
Shruti Lad <ladshruti73@gmail.com>
2020-06-19 12:58:20 +0530

    commiting changes

--- Scripts/Custom/load-template.js
+++ Scripts/Custom/load-template.js
@@ -1,8 +1,12 @@
-﻿function load_blogs() {
-    $("#pages-placeholder").load("../../Pages/Site/blogs.html");
-}
+﻿
 function load_home() {
     $("#pages-placeholder").load("../../Pages/Site/home.html");
+}
+function load_projects() {
+    $("#pages-placeholder").load("../../Pages/Site/blogs.html");
+}
+function load_blogs() {
+    $("#pages-placeholder").load("../../Pages/Site/blogs.html");
 }
 function load_contact() {
     $("#pages-placeholder").load("../../Pages/Site/contact.html");
