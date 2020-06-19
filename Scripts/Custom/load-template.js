4e18e06fff879caf9a4c3f19337ca60c58780996
dushpethkar <dushpethkar@gmail.com>
2020-06-19 12:53:36 +0530

    New Updates

--- Scripts/Custom/load-template.js
+++ Scripts/Custom/load-template.js
@@ -1,12 +1,9 @@
-﻿
-
-function load_home() {
+﻿function load_home() {
     $("#pages-placeholder").load("../../Pages/Site/home.html");
 }
 function load_projects() {
     $("#pages-placeholder").load("../../Pages/Site/projects.html");
 }
-
 function load_blogs() {
     $("#pages-placeholder").load("../../Pages/Site/blogs.html");
 }
